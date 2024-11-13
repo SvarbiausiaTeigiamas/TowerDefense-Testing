d3cf28fc68d10e360dd30e64bfa446d1530c63fa
arnasbrad <arnas1373@gmail.com>
2024-11-13 22:39:38 +0200

    finished tests

--- src/TowerDefense.Tests/IntegrationTest.cs
+++ src/TowerDefense.Tests/IntegrationTest.cs
@@ -5,47 +5,13 @@ using Microsoft.AspNetCore.Mvc.Testing;
 using TowerDefense.Api.Bootstrap;
 using TowerDefense.Api.Bootstrap.AutoMapper;
 using TowerDefense.Api.Constants;
-using TowerDefense.Api.Contracts.Inventory;
+using TowerDefense.Api.Contracts.Grid;
 using TowerDefense.Api.Contracts.Player;
 using TowerDefense.Api.Contracts.Shop;
-using TowerDefense.Api.GameLogic.Items;
-using TowerDefense.Api.GameLogic.Items.Models;
+using TowerDefense.Api.Contracts.Turn;
 using Xunit.Abstractions;
 
 namespace UnitTests;
-
-public class CustomWebApplicationFactory : WebApplicationFactory<Program>
-{
-    protected override void ConfigureWebHost(IWebHostBuilder builder)
-    {
-        builder.UseUrls("http://localhost:5042", "https://localhost:7042");
-
-        builder.ConfigureServices(services =>
-        {
-            services.AddControllers();
-            services.AddEndpointsApiExplorer();
-            services.AddSwaggerGen();
-            services.AddSignalR();
-            services.SetupGameEngine();
-            services.SetupAutoMapper();
-
-            services.AddCors(options =>
-            {
-                options.AddPolicy(
-                    Policy.DevelopmentCors,
-                    b =>
-                    {
-                        b.WithOrigins("https://localhost:3000")
-                            .AllowAnyHeader()
-                            .AllowAnyMethod()
-                            .SetIsOriginAllowed((_) => true)
-                            .AllowCredentials();
-                    }
-                );
-            });
-        });
-    }
-}
 
 public class GameIntegrationTests : IClassFixture<CustomWebApplicationFactory>
 {
@@ -65,7 +31,14 @@ public class GameIntegrationTests : IClassFixture<CustomWebApplicationFactory>
     }
 
     [Fact]
-    public async Task AddPlayers_OnGameStart_PlayersAddedSuccessfully()
+    public async Task TestGameWorkflow()
+    {
+        await AddPlayers_OnGameStart_PlayersAddedSuccessfully();
+        await BuyAndPlaceItems_RocketAndShield_ItemsBoughtAndPlacedSuccessfully();
+        await EndTurns_UntilGameFinished_PlayerReachedZeroHealth();
+    }
+
+    private async Task AddPlayers_OnGameStart_PlayersAddedSuccessfully()
     {
         // Register both players
         var player1Response = await _client.PostAsJsonAsync(
@@ -104,8 +77,7 @@ public class GameIntegrationTests : IClassFixture<CustomWebApplicationFactory>
             .BeEquivalentTo(await player2Info.Content.ReadFromJsonAsync<GetPlayerInfoResponse>());
     }
 
-    [Fact]
-    public async Task BuyItems_RocketAndShield_ItemsBoughtSuccessfully()
+    private async Task BuyAndPlaceItems_RocketAndShield_ItemsBoughtAndPlacedSuccessfully()
     {
         var player1BuyItemResponse = await _client.PostAsJsonAsync(
             "/api/shop",
@@ -120,19 +92,123 @@ public class GameIntegrationTests : IClassFixture<CustomWebApplicationFactory>
         Assert.Equal(HttpStatusCode.OK, player2BuyItemResponse.StatusCode);
 
         var player1Items = await _client.GetAsync("/api/inventory/PlayerOne");
+        var player1ItemsBody = await player1Items.Content.ReadAsStringAsync();
         JsonDocument
-            .Parse(await player1Items.Content.ReadAsStringAsync())
+            .Parse(player1ItemsBody)
             .RootElement.GetProperty("items")
             .GetArrayLength()
             .Should()
             .Be(1);
 
         var player2Items = await _client.GetAsync("/api/inventory/PlayerTwo");
+        var player2ItemsBody = await player2Items.Content.ReadAsStringAsync();
         JsonDocument
-            .Parse(await player2Items.Content.ReadAsStringAsync())
+            .Parse(player2ItemsBody)
             .RootElement.GetProperty("items")
             .GetArrayLength()
             .Should()
             .Be(1);
+
+        var player1ItemId = JsonDocument
+            .Parse(player1ItemsBody)
+            .RootElement.GetProperty("items")[0]
+            .GetProperty("id")
+            .GetString();
+        var player2ItemId = JsonDocument
+            .Parse(player2ItemsBody)
+            .RootElement.GetProperty("items")[0]
+            .GetProperty("id")
+            .GetString();
+
+        var player1PlaceItemResponse = await _client.PostAsJsonAsync(
+            "/api/players/place-item",
+            new AddGridItemRequest
+            {
+                GridItemId = 36,
+                PlayerName = "PlayerOne",
+                InventoryItemId = player1ItemId,
+            }
+        );
+        var player2PlaceItemResponse = await _client.PostAsJsonAsync(
+            "/api/players/place-item",
+            new AddGridItemRequest
+            {
+                GridItemId = 36,
+                PlayerName = "PlayerOne",
+                InventoryItemId = player2ItemId,
+            }
+        );
+
+        Assert.Equal(HttpStatusCode.OK, player1PlaceItemResponse.StatusCode);
+        Assert.Equal(HttpStatusCode.OK, player2PlaceItemResponse.StatusCode);
+    }
+
+    private async Task EndTurns_UntilGameFinished_PlayerReachedZeroHealth()
+    {
+        async Task EndTurns()
+        {
+            var first = await _client.PostAsJsonAsync(
+                "/api/players/endturn",
+                new EndTurnRequest { PlayerName = "PlayerOne" }
+            );
+            var second = await _client.PostAsJsonAsync(
+                "/api/players/endturn",
+                new EndTurnRequest { PlayerName = "PlayerTwo" }
+            );
+
+            Assert.Equal(HttpStatusCode.OK, first.StatusCode);
+            Assert.Equal(HttpStatusCode.OK, second.StatusCode);
+        }
+
+        await EndTurns();
+        await EndTurns();
+        await EndTurns();
+
+        var player2InfoResp = await _client.GetAsync("/api/players/PlayerTwo");
+        Assert.Equal(HttpStatusCode.OK, player2InfoResp.StatusCode);
+
+        var player2Info = await player2InfoResp.Content.ReadFromJsonAsync<GetPlayerInfoResponse>();
+        Assert.Equal(20, player2Info.Health);
+
+        await EndTurns();
+
+        player2InfoResp = await _client.GetAsync("/api/players/PlayerTwo");
+        Assert.Equal(HttpStatusCode.OK, player2InfoResp.StatusCode);
+
+        player2Info = await player2InfoResp.Content.ReadFromJsonAsync<GetPlayerInfoResponse>();
+        Assert.Equal(0, player2Info.Health);
+    }
+}
+
+public class CustomWebApplicationFactory : WebApplicationFactory<Program>
+{
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.UseUrls("http://localhost:5042", "https://localhost:7042");
+
+        builder.ConfigureServices(services =>
+        {
+            services.AddControllers();
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+            services.AddSignalR();
+            services.SetupGameEngine();
+            services.SetupAutoMapper();
+
+            services.AddCors(options =>
+            {
+                options.AddPolicy(
+                    Policy.DevelopmentCors,
+                    b =>
+                    {
+                        b.WithOrigins("https://localhost:3000")
+                            .AllowAnyHeader()
+                            .AllowAnyMethod()
+                            .SetIsOriginAllowed((_) => true)
+                            .AllowCredentials();
+                    }
+                );
+            });
+        });
     }
 }
